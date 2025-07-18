285e3a2504a6a8cd4d6d55b721ef3e837e3b3073
Felipe Lagos <felagossanchez@gmail.com>
2025-07-17 21:21:15 -0400

    Implement CRUD operations in ProductController

--- src/main/java/com/app/cqrs/product/controller/ProductController.java
+++ src/main/java/com/app/cqrs/product/controller/ProductController.java
@@ -1,10 +1,32 @@
 package com.app.cqrs.product.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @PostMapping
+    public String createProduct() {
+        return "Product created successfully!";
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "Product details";
+    }
+
+    @PostMapping
+    public String updateProduct() {
+        return "Product updated successfully!";
+    }
+
+    @PostMapping
+    public String deleteProduct() {
+        return "Product deleted successfully!";
+    }
 
 }
