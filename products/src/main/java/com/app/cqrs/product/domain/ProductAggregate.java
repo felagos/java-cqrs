a6ffc05655b47290849a4bba6e3d188670310681
Felipe Lagos <felagossanchez@gmail.com>
2025-08-17 15:23:46 -0400

    feat: Add annotations for CommandHandler and Aggregate to ProductAggregate class

--- products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
+++ products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
@@ -1,12 +1,16 @@
 package com.app.cqrs.product.domain;
 
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.spring.stereotype.Aggregate;
 import com.app.cqrs.product.domain.commands.CreateProductCommand;
 
+@Aggregate
 public class ProductAggregate {
 
     public ProductAggregate() {
     }
 
+    @CommandHandler
     public ProductAggregate(CreateProductCommand command) {
     }
 
