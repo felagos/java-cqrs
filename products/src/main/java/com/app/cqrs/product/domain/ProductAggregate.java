c3bdfa691fd28d620cded2f2301d3afa27f1082b
Felipe Lagos <felagossanchez@gmail.com>
2025-08-17 15:31:03 -0400

    feat: Add validation for product price in ProductAggregate and create InvalidProductException

--- products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
+++ products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
@@ -1,5 +1,7 @@
 package com.app.cqrs.product.domain;
 
+import java.math.BigDecimal;
+
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 import com.app.cqrs.product.domain.commands.CreateProductCommand;
@@ -12,6 +14,9 @@ public class ProductAggregate {
 
     @CommandHandler
     public ProductAggregate(CreateProductCommand command) {
+        if (command.price().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Invalid product price");
+        }
     }
 
 }
