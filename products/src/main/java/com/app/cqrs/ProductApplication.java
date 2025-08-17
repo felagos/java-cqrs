2cd04ab61c264e57c20421f5ae447594d2036b0a
Felipe Lagos <felagossanchez@gmail.com>
2025-08-17 17:48:14 -0400

    feat: Import XStreamConfig for XML serialization support in ProductApplication

--- products/src/main/java/com/app/cqrs/ProductApplication.java
+++ products/src/main/java/com/app/cqrs/ProductApplication.java
@@ -3,9 +3,13 @@ package com.app.cqrs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
+
+import com.app.cqrs.product.infrastructure.configuration.XStreamConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(XStreamConfig.class)
 public class ProductApplication {
 
 	public static void main(String[] args) {
