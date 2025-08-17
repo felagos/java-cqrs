3f9bfbb59287db335c0d68289548ebc8e67da67e
Felipe Lagos <felagossanchez@gmail.com>
2025-08-17 17:42:57 -0400

    feat: Implement ProductAggregate to handle product creation and events

--- products/src/main/java/com/app/cqrs/product/domain/aggregates/ProductAggregate.java
+++ products/src/main/java/com/app/cqrs/product/domain/aggregates/ProductAggregate.java
@@ -1,8 +1,10 @@
-package com.app.cqrs.product.domain;
+package com.app.cqrs.product.domain.aggregates;
 
 import java.math.BigDecimal;
 
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
 import com.app.cqrs.product.domain.commands.CreateProductCommand;
@@ -10,6 +12,12 @@ import com.app.cqrs.product.domain.events.ProductCreatedEvent;
 
 @Aggregate
 public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
 
     public ProductAggregate() {
     }
@@ -28,6 +36,14 @@ public class ProductAggregate {
         );
 
         AggregateLifecycle.apply(productCreatedEvent);
+    }
+
+    @EventSourcingHandler
+    public void on(ProductCreatedEvent event) {
+        this.productId = event.productId();
+        this.title = event.title();
+        this.price = event.price();
+        this.quantity = event.quantity();
     }
 
 }
