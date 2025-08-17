c2cf6206d6fe709f08543b964bd69d34008679fe
Felipe Lagos <felagossanchez@gmail.com>
2025-08-17 15:22:45 -0400

    feat: Add overloaded constructor for ProductAggregate to initialize with CreateProductCommand

--- products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
+++ products/src/main/java/com/app/cqrs/product/domain/ProductAggregate.java
@@ -1,5 +1,13 @@
 package com.app.cqrs.product.domain;
 
+import com.app.cqrs.product.domain.commands.CreateProductCommand;
+
 public class ProductAggregate {
+
+    public ProductAggregate() {
+    }
+
+    public ProductAggregate(CreateProductCommand command) {
+    }
 
 }
