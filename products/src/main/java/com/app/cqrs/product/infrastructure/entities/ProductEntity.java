9522e3bb1e809be5763e7c178120fb76abbeda4f
Felipe Lagos <felagossanchez@gmail.com>
2025-08-18 14:14:51 -0400

    refactor: Move getter and setter methods to the end of ProductEntity class for better organization

--- products/src/main/java/com/app/cqrs/product/infrastructure/entities/ProductEntity.java
+++ products/src/main/java/com/app/cqrs/product/infrastructure/entities/ProductEntity.java
@@ -24,4 +24,36 @@ public class ProductEntity {
     @Column(name = "quantity", nullable = false)
     private int quantity;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
