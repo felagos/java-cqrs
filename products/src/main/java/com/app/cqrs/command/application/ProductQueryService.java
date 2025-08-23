7cfb86a3a7186a64ea25645d619def7704a1ad8c
Felipe Lagos <felagossanchez@gmail.com>
2025-08-23 16:46:24 -0400

    feat: Add ProductQueryService and integrate with ProductQueryController

---
+++ products/src/main/java/com/app/cqrs/command/application/ProductQueryService.java
@@ -0,0 +1,8 @@
+package com.app.cqrs.command.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductQueryService {
+
+}
--- products/src/main/java/com/app/cqrs/query/infrastructure/controllers/ProductQueryController.java
+++ products/src/main/java/com/app/cqrs/query/infrastructure/controllers/ProductQueryController.java
@@ -4,9 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.cqrs.command.application.ProductQueryService;
+
 @RestController
 @RequestMapping("/products")
 public class ProductQueryController {
+
+    private final ProductQueryService productQueryService;
+
+    public ProductQueryController(ProductQueryService productQueryService) {
+        this.productQueryService = productQueryService;
+    }
 
     @GetMapping
     public String getProduct() {
