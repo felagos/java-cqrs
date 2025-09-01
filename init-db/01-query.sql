e6f1e31472869ffd1c4703bd8a573a8f9e6a0840
Felipe Lagos <felagossanchez@gmail.com>
2025-09-01 08:22:22 -0400

    feat: Revise database initialization scripts and update healthcheck for PostgreSQL service

--- init-db/01-query.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS products;
-
-CREATE TABLE products (
-    id varchar(36) NOT NULL,
-    title varchar(255) NOT NULL,
-    price decimal(10,2) NOT NULL,
-    quantity int NOT NULL,
-    PRIMARY KEY (id)
-);
---
+++ init-db/init.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id varchar(36) NOT NULL,
+    title varchar(255) NOT NULL,
+    price decimal(10,2) NOT NULL,
+    quantity int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+/* TRUNCATE TABLE IF EXISTS products; */
+
+INSERT INTO products (id,price,quantity,title) VALUES
+	 ('cda771ec-1080-416c-bad1-5bf49fe72df6',1100000.00,2,'iphone 16'),
+	 ('85a6a910-2e49-4314-9662-407a718d9586',1100000.00,2,'iphone 15'),
+	 ('60877f7e-9ee6-4dba-866d-50a72713f62b',1200000.00,3,'iphone 14');
