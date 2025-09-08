f21c432dde1204c5998c5b2b6c5d3d5d4ff4b058
Felipe Lagos <felagossanchez@gmail.com>
2025-09-07 22:09:19 -0300

    refactor: Simplify CreateProductCommandInterceptor by removing unused repository and exception handling

--- products/src/main/java/com/app/cqrs/command/infrastructure/interceptors/CreateProductCommandInterceptor.java
+++ products/src/main/java/com/app/cqrs/command/infrastructure/interceptors/CreateProductCommandInterceptor.java
@@ -8,19 +8,11 @@ import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.messaging.MessageDispatchInterceptor;
 import org.springframework.stereotype.Component;
 import com.app.cqrs.command.domain.commands.CreateProductCommand;
-import com.app.cqrs.command.domain.exceptions.ExistingProductException;
-import com.app.cqrs.command.infrastructure.repositories.ProductLookupRepository;
 
 @Component
 public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
 
     private static final Logger logger = Logger.getLogger(CreateProductCommandInterceptor.class.getName());
-
-    private final ProductLookupRepository productLookupRepository;
-
-    public CreateProductCommandInterceptor(ProductLookupRepository productLookupRepository) {
-        this.productLookupRepository = productLookupRepository;
-    }
 
     @Override
     public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(
@@ -31,14 +23,6 @@ public class CreateProductCommandInterceptor implements MessageDispatchIntercept
 
                 if (command.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
                     this.logger.warning("Invalid product price: " + command.getPrice());
-                }
-
-                var existProduct = this.productLookupRepository.existsByIdOrTitle(command.getProductId(),
-                        command.getTitle());
-
-                if (existProduct) {
-                    throw new ExistingProductException("Product with title " + command.getTitle() + " and id "
-                            + command.getProductId() + " already exists.");
                 }
 
                 this.logger.info("Creating product: " + command.getTitle() + " with price: " + command.getPrice());
