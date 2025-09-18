7cbef5aa3d0b30b5b01412bb3bd1209662a8d792
Felipe Lagos <felagossanchez@gmail.com>
2025-09-18 19:43:25 -0300

    feat: End saga lifecycle after order approval in OrderSaga

--- products/src/main/java/com/app/cqrs/command/domain/saga/OrderSaga.java
+++ products/src/main/java/com/app/cqrs/command/domain/saga/OrderSaga.java
@@ -5,6 +5,7 @@ import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.modelling.saga.EndSaga;
 import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.SagaLifecycle;
 import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -131,6 +132,8 @@ public class OrderSaga {
         }
 
         this.getLogger().info("Ending saga for order: " + approvedEvent.getOrderId());
+
+        SagaLifecycle.end();
     }
 
 }
