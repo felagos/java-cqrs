ff6c871718b979324e2cb69ad9c17d55a6af59be
Felipe Lagos <felagossanchez@gmail.com>
2025-09-18 17:24:55 -0300

    feat: Add ApproveOrderCommand class for order approval functionality

---
+++ products/src/main/java/com/app/cqrs/command/domain/commands/ApproveOrderCommand.java
@@ -0,0 +1,21 @@
+package com.app.cqrs.command.domain.commands;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public class ApproveOrderCommand {
+
+    @TargetAggregateIdentifier
+    private String orderId;
+
+    public ApproveOrderCommand() {
+    }
+
+    public ApproveOrderCommand(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+}
