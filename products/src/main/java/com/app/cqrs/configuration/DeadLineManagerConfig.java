1ae825a6d08faaa865424fda1e13d9c0aa20d5bf
Felipe Lagos <felagossanchez@gmail.com>
2025-09-20 17:36:56 -0300

    feat: Add DeadlineManager configuration for handling deadlines in the application

---
+++ products/src/main/java/com/app/cqrs/configuration/DeadLineManagerConfig.java
@@ -0,0 +1,23 @@
+package com.app.cqrs.configuration;
+
+import org.axonframework.config.ConfigurationScopeAwareProvider;
+import org.axonframework.deadline.DeadlineManager;
+import org.axonframework.deadline.SimpleDeadlineManager;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DeadLineManagerConfig {
+
+    @Bean
+    public DeadlineManager getDeadlineManager(org.axonframework.config.Configuration configuration,
+            SpringTransactionManager transactionManager) {
+        return SimpleDeadlineManager
+                .builder()
+                .scopeAwareProvider(new ConfigurationScopeAwareProvider(configuration))
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+}
