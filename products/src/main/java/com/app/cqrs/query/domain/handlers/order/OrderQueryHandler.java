d3dabcf84343e069ab2abb094883c7a59347f810
Felipe Lagos <felagossanchez@gmail.com>
2025-09-21 16:16:54 -0300

    refactor: Remove unused fields and annotations from FindProductQuery; add OrderQueryHandler class

---
+++ products/src/main/java/com/app/cqrs/query/domain/handlers/order/OrderQueryHandler.java
@@ -0,0 +1,8 @@
+package com.app.cqrs.query.domain.handlers.order;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderQueryHandler {
+
+}
--- products/src/main/java/com/app/cqrs/query/domain/queries/product/FindProductQuery.java
+++ products/src/main/java/com/app/cqrs/query/domain/queries/product/FindProductQuery.java
@@ -1,14 +1,5 @@
 package com.app.cqrs.query.domain.queries.product;
 
-import java.math.BigDecimal;
-
-import lombok.Value;
-
-@Value
 public class FindProductQuery {
-
-    private final String title;
-    private final BigDecimal minPrice;
-    private final BigDecimal maxPrice;
 
 }
