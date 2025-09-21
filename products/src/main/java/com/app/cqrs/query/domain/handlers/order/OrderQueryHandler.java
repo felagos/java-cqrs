2b9da7365873a9abe79d38e8e5995310de1f2ae5
Felipe Lagos <felagossanchez@gmail.com>
2025-09-21 16:18:31 -0300

    refactor: Implement OrderQueryHandler with findOrder method and repository dependency

--- products/src/main/java/com/app/cqrs/query/domain/handlers/order/OrderQueryHandler.java
+++ products/src/main/java/com/app/cqrs/query/domain/handlers/order/OrderQueryHandler.java
@@ -1,8 +1,21 @@
 package com.app.cqrs.query.domain.handlers.order;
 
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
+
+import com.app.cqrs.command.domain.ports.orders.IOrderRepository;
+import com.app.cqrs.query.domain.queries.order.FindOrderQuery;
 
 @Component
 public class OrderQueryHandler {
+
+    private final IOrderRepository orderRepository;
+
+    public OrderQueryHandler(IOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @QueryHandler
+    public void findOrder(FindOrderQuery query) {}
 
 }
